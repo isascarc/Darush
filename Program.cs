9433e04739716a48a15e5546f80e67a4046343dd
isascarc <isascarch@gmail.com>
2023-08-14 13:23:36 +0300

    Add Exposed Header

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200")
+    .WithExposedHeaders("Content-Disposition"));
 
 app.UseAuthentication();
 app.UseAuthorization();
