73d432ee7bf3be55f31b15a77815f80cf88ace55
isascarc <isascarch@gmail.com>
2023-08-23 20:15:03 +0300

    Fix dto names

--- DTOs/JobDto.cs
+++ DTOs/JobDto.cs
@@ -1,12 +1,12 @@
 namespace MyJob.DTOs;
 public class JobDto
 {
-    public int salary { get; set; }
-    public bool haveEnglish { get; set; }
-    public string profession { get; set; }
-    public string toppings { get; set; }
-    public bool haveToar { get; set; }
-    public string jobDetails { get; set; }
+    public int Salary { get; set; }
+    public bool HaveEnglish { get; set; }
+    public string Profession { get; set; }
+    public string Toppings { get; set; }
+    public bool HaveToar { get; set; }
+    public string JobDetails { get; set; }
     public int Area { get; set; }
     // public string other { get; set; }
 }
