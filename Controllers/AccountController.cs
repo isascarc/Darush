bbe1d01bc80fcdfaa40d6e32e1f07b3d1c072f7a
isascarc <isascarch@gmail.com>
2023-08-30 12:46:18 +0300

    Add WebsiteLink

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -145,6 +145,8 @@ public class AccountController : BaseApiController
         var user = (await GetUserInfo());
 
         memberUpdateDto.Adapt(user);
+
+        // TODO: send new token if userName changed
         return (await _context.SaveChangesAsync()) > 0 ? NoContent() : BadRequest("failed to update user.");
     }
 
--- DTOs/MemberUpdateDto.cs
+++ DTOs/MemberUpdateDto.cs
@@ -2,13 +2,13 @@ namespace MyJob.DTOs;
 
 public class MemberUpdateDto
 {
-    public string userName { get; set; }
+    public string UserName { get; set; }
     // update pass
     
     public string Mail { get; set; }
     public string City { get; set; }
     public string Phone { get; set; }
     public string LinkedinLink { get; set; }
-    //public string WebsiteLink { get; set; }
+    public string WebsiteLink { get; set; }
     //todo:  public DateOnly DateOfBirth { get; set; }
 }
