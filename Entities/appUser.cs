9141cbdd31ec74a4ebe7c85104d435bf0f7dc537
isascarc <isascarch@gmail.com>
2023-08-31 08:52:13 +0300

    Add [Required] to AppUser

--- Entities/appUser.cs
+++ Entities/appUser.cs
@@ -1,15 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyJob.Entities;
 
 public class AppUser
 {
-    public int Id { get; set; }
-    public string UserName { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
+    [Required] public int Id { get; set; }
+    [Required] public string UserName { get; set; }
+    [Required] public byte[] PasswordHash { get; set; }
+    [Required] public byte[] PasswordSalt { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public string KnownAs { get; set; }
-    public DateTime Create { get; set; } = DateTime.UtcNow;
-    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    [Required] public DateTime Create { get; set; } = DateTime.UtcNow;
+    [Required] public DateTime LastActive { get; set; } = DateTime.UtcNow;
     public string Gender { get; set; }        
     public string City { get; set; }
     public string Mail { get; set; }
