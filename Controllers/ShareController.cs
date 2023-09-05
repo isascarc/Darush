a2b84b9469d94d8cb8bf529c2038136de4d32847
isascarc <isascarch@gmail.com>
2023-09-05 10:04:53 +0300

    R

--- Controllers/ShareController.cs
+++ Controllers/ShareController.cs
@@ -8,6 +8,7 @@ public class ShareController : BaseApiController
 {
     public DataContext _context { get; }
     public ITokenService _tokenService { get; }
+    const string ourEmailAddress = "isscr01@gmail.com";
 
     public ShareController(DataContext context, ITokenService tokenService)
     {
@@ -19,22 +20,24 @@ public class ShareController : BaseApiController
     public async Task<ActionResult<UserDto>> Create(SendMailDto emailDetails)
     {
         var user = (await GetUser());
+        var job = _context.Jobs.First(x => x.Id == emailDetails.jobNumber);
+
 
         var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("Darush", "isscr01@gmail.com"));
+        message.From.Add(new MailboxAddress("Darush", ourEmailAddress));
         message.To.Add(new MailboxAddress(user.UserName, emailDetails.emailAddress.Length > 0 ? emailDetails.emailAddress : user.Mail));
         message.Subject = "יש לנו משרה חדשה עבורך!!";
 
 
         message.Body = new TextPart("plain")
         {
-            Text = $"בקשת לשלוח את המשרה הזו אלייך למייל. מספר המשרה הוא: {emailDetails.jobNumber}. בהצלחה!!"
+            Text = $"בקשת לשלוח את המשרה הזו אלייך למייל. מספר המשרה הוא: {emailDetails.jobNumber}. תוכן המשרה הוא: {job.text} בהצלחה!!"
         };
 
         using (var client = new MailKit.Net.Smtp.SmtpClient())
         {
             client.Connect("smtp.gmail.com", 587, false);
-            client.Authenticate("isscr01@gmail.com", Globals.GmailCode);
+            client.Authenticate(ourEmailAddress, Globals.GmailCode);
             var a = client.Send(message);
             client.Disconnect(true);
         }
