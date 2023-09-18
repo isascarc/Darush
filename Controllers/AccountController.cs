fda9ef91ddaafed56d4eb841c3280f0eb099580c
isascarc <isascarch@gmail.com>
2023-09-18 21:20:41 +0300

    R

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -14,12 +14,22 @@ public class AccountController : BaseApiController
         _tokenService = tokenService;
     }
 
-    #region Register & login   
+    #region Register & login
     [AllowAnonymous]
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register2(RegisterDto2 registerDto)
     {
         return await Register(new RegisterDto() { username = registerDto.username, password = registerDto.password });
+
+        /*
+        התקבלתי
+
+        מה שואלים?
+        כמה זמן?
+
+
+
+         */
         //if (await UserExist(registerDto.username))
         //    return BadRequest("username is taken");
 
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -10,23 +10,6 @@ namespace MyJob.Services
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
-        //public string CreateToken(AppUser user)
-        //{
-        //    var claims = new List<Claim>
-        //    {
-        //        new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
-        //    };
-        //    var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-        //    var tokenDescriptor = new SecurityTokenDescriptor
-        //    {
-        //        Subject = new ClaimsIdentity(claims),
-        //        Expires = DateTime.Now.AddDays(7),
-        //        SigningCredentials = creds
-        //    };
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-        //    var token = tokenHandler.CreateToken(tokenDescriptor);
-        //    return tokenHandler.WriteToken(token);
-        //}
 
         public string CreateToken(string UserName)
         {
@@ -45,23 +28,5 @@ namespace MyJob.Services
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
-
-        //public string CreateTokenForRec(Recruiter user)
-        //{
-        //    var claims = new List<Claim>
-        //    {
-        //        new Claim(JwtRegisteredClaimNames.NameId, user.RecName)
-        //    };
-        //    var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-        //    var tokenDescriptor = new SecurityTokenDescriptor
-        //    {
-        //        Subject = new ClaimsIdentity(claims),
-        //        Expires = DateTime.Now.AddDays(7),
-        //        SigningCredentials = creds
-        //    };
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-        //    var token = tokenHandler.CreateToken(tokenDescriptor);
-        //    return tokenHandler.WriteToken(token);
-        //}
     }
 }
